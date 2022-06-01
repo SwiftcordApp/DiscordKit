f44f1853fdf626f948981ab9990d59629adb5295
Tony Arnold <tony@thecocoabots.com>
2022-06-01 14:07:15 +1000

    Refine the types vended by the User type and it’s related entities

---
+++ Sources/DiscordKit/Objects/User+PremiumType.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public extension User {
+    enum PremiumType: Int, Codable, Hashable, Identifiable, CustomStringConvertible {
+        /// No premium subscription
+        case none = 0
+
+        /// Nitro classic
+        case nitroClassic = 1
+
+        /// Nitro
+        case nitro = 2
+
+        // MARK:  Identifiable
+
+        public var id: RawValue {
+            return rawValue
+        }
+
+        // MARK: CustomStringConvertible
+
+        public var description: String {
+            switch self {
+                case .none:
+                    return "None"
+
+                case .nitroClassic:
+                    return "Nitro Classic"
+
+                case .nitro:
+                    return "Nitro"
+            }
+        }
+    }
+}
--- Sources/DiscordKit/Objects/User.swift
+++ Sources/DiscordKit/Objects/User.swift
@@ -1,4 +1,3 @@
-//
 //  User.swift
 //  DiscordAPI
 //
@@ -7,36 +6,9 @@
 
 import Foundation
 
-public enum UserFlags: Int, CaseIterable {
-    case staff = 0
-    case partner = 1
-    case hypesquadEvents = 2
-    case bugHunterLv1 = 3
-    case hypesquadBravery = 6
-    case hypesquadBrilliance = 7
-    case hypesquadBalance = 8
-    case earlySupporter = 9
-    case teamPseudoUser = 10
-    case bugHunterLv2 = 14
-    case verifiedBot = 16
-    case verifiedDev = 17
-    case certifiedMod = 18
-    case botHTTPInteractions = 19
-    case premium = 50 // Not actually a possible flag, but included for use in arrays
-}
-
-public enum PremiumType: Int, Codable {
-    /// No premium subscription
-    case none = 0
-    /// Nitro classic
-    case nitroClassic = 1
-    /// Nitro
-    case nitro = 2
-}
-
 public struct User: Codable, GatewayData, Equatable {
     // To work around the default access level
-    public init(id: Snowflake, username: String, discriminator: String, avatar: String?, bot: Bool?, bio: String?, system: Bool?, mfa_enabled: Bool?, banner: String?, accent_color: Int?, locale: Locale?, verified: Bool?, flags: Int?, premium_type: PremiumType?, public_flags: Int?) {
+    public init(id: Snowflake, username: String, discriminator: String, avatar: String?, bot: Bool?, bio: String?, system: Bool?, mfa_enabled: Bool?, banner: String?, accent_color: Int?, locale: Locale?, verified: Bool?, flags: User.Flags?, premium_type: User.PremiumType?, public_flags: User.Flags?) {
         self.id = id
         self.username = username
         self.discriminator = discriminator
@@ -53,57 +25,57 @@ public struct User: Codable, GatewayData, Equatable {
         self.premium_type = premium_type
         self.public_flags = public_flags
     }
-    
+
     /// ID of this user
     public let id: Snowflake
-    
+
     /// Username of this user
     public let username: String
-    
+
     /// Discriminator of this user
     ///
     /// A string in the format #0000
     public let discriminator: String
-    
+
     /// User's avatar hash
     public let avatar: String?
-    
+
     /// If this user is a bot
     public let bot: Bool?
-    
+
     /// Bio of this user
     public let bio: String?
-    
+
     /// If this user is a system user
     public let system: Bool?
-    
+
     /// If this user has 2FA enabled on their account
     public let mfa_enabled: Bool?
-    
+
     /// Banner image hash (nitro-only)
     public let banner: String?
-    
+
     /// Banner color
     public let accent_color: Int?
-    
+
     /// Locale of this user
     public let locale: Locale?
-    
+
     /// If the user's email is verified
     public let verified: Bool?
-    
+
     /// The flags of this user
     ///
     /// Use ``User/flagsArr`` for a decoded array of ``UserFlags``.
-    public let flags: Int?
-    
+    public let flags: User.Flags?
+
     /// The public flags of this user
     ///
     /// > This appears to always be the same as ``CurrentUser/flags``
-    public let public_flags: Int?
-    
+    public let public_flags: User.Flags?
+
     /// The type of nitro subscription this user has
-    public let premium_type: PremiumType?
+    public let premium_type: User.PremiumType?
 }
 
 /// A subset of the ``User`` struct with less properties and less optionals
@@ -117,62 +89,60 @@ public struct CurrentUser: Codable, GatewayData, Equatable {
     /// > Will only be present for the current user, and is nil
     /// > if the user did not add a number to the account
     public let phone: String?
-    
+
     /// Email associated with this account
     ///
     /// > Will only be present for the current user
     public let email: String
-    
+
     /// ID of the current user
     public let id: Snowflake
-    
+
     /// Username of this user
     public let username: String
-    
+
     /// Discriminator of this user
     ///
     /// A string in the format #0000
     public let discriminator: String
-    
+
     /// The flags of this user
     ///
     /// Use ``CurrentUser/flagsArr`` for a decoded array of ``UserFlags``.
-    public let flags: Int
-    
+    public let flags: User.Flags
+
     /// The public flags of this user
-    ///
     /// > This appears to always be the same as ``CurrentUser/flags``
-    public let public_flags: Int?
-    
+    public let public_flags: User.Flags?
+
     /// The user's purchased flags
-    ///
     /// (Guess: This is equivalent to ``User/premium_type``)
-    public let purchased_flags: PremiumType?
-    
+    public let purchased_flags: User.PremiumType?
+
     /// If this user is a premium (nitro) user
     public let premium: Bool
-    
+
     /// If this user has consented to viewing 18+ (NSFW) channels
     public let nsfw_allowed: Bool?
-    
+
     /// If this user has logged in from a mobile client before
     public let mobile: Bool
-    
+
     /// If this user has logged in from a desktop desktop before
     public let desktop: Bool
-    
+
     /// If the user has 2FA enabled on their account
     public let mfa_enabled: Bool
-    
+
     /// Bio of this user
     public let bio: String?
-    
+
     /// Banner color
     public let accent_color: Int?
-    
+
     /// Banner image hash (nitro-only)
     public let banner: String?
-    
+
     /// User's avatar hash
     public let avatar: String?
 }
@@ -190,35 +160,15 @@ public struct UserProfile: Codable, GatewayData {
         self.mutual_guilds = mutual_guilds
         self.user = user
     }
-    
+
     public let connected_accounts: [Connection]
     public let guild_member: Member?
     public let premium_guild_since: ISOTimestamp?
     public let premium_since: ISOTimestamp?
     public let mutual_guilds: [MutualGuild]?
-    
+
     /// A more complete ``User`` struct, containing the user's bio, among others.
     public let user: User
-}
-
-public extension User {
-    /// Get the decoded array of flags
-    var flagsArr: [UserFlags]? {
-        guard var decodedFlags = flags?.decodeFlags(flags: UserFlags.staff) else {
-            return nil
-        }
-        if let premiumType = premium_type, premiumType != .none {
-            decodedFlags.append(.premium)
-        }
-        return decodedFlags
-    }
-}
-public extension CurrentUser {
-    var flagsArr: [UserFlags] {
-        var decodedFlags = flags.decodeFlags(flags: UserFlags.staff)
-        if premium { decodedFlags.append(.premium) }
-        return decodedFlags
-    }
 }
 
 public extension User {
@@ -238,7 +188,7 @@ public extension User {
             locale: nil,
             verified: nil,
             flags: user.flags,
-            premium_type: user.premium ? user.purchased_flags : PremiumType.none,
+            premium_type: user.premium ? user.purchased_flags : User.PremiumType.none,
             public_flags: user.public_flags
         )
     }
