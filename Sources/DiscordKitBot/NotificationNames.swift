a886763c6c887abdc7b88e77de43253d2a7d579a
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-11-23 21:06:06 +0800

    feat (bot): add initial notification support

---
+++ Sources/DiscordKitBot/NotificationNames.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationNames.swift
+//  
+//
+//  Created by Vincent Kwok on 23/11/22.
+//
+
+import Foundation
+
+public extension NSNotification.Name {
+    static let ready = Self("dk-ready")
+}
