2fb2d36d11ca97bcb66fda2e02daca4f5b1b8f96
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-12-12 12:07:59 +0800

    patch (rest/types): change structure of CreateAppCmd struct
    * added extensive DocC comments
    * moved enums around to better locations
    * created enums for storing fields which may have multiple possible types

--- Sources/DiscordKitCore/Objects/REST/CreateAppCmd.swift
+++ Sources/DiscordKitCore/Objects/REST/CreateAppCmd.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 /// Payload sent to the create application command endpoint to create an application command
+///
+/// > Application commands are known as "slash commands" in the Discord client.
 public struct CreateAppCmd {
+    public init(name: String, description: String = "", options: [AppCommandOption]? = nil) {
+        self.name = name
+        self.description = description
+        self.options = options
+    }
+
     public let name: String
-    public let description: String?
+    public let description: String
     public let options: [AppCommandOption]?
 }
