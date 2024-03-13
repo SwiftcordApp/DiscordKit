cdd577e16d97b945b7bb65530e336eaeeea47e24
cryptoAlgorithm <cryptoalgro@gmail.com>
2024-03-13 23:14:12 +0800

    patch(user): correct placeholder avatar logic for migrated users
    also added avatar extension for CurrentUser, although simply duplicating the method is less than ideal

--- Sources/DiscordKitCore/Extensions/Objects/User+.swift
+++ Sources/DiscordKitCore/Extensions/Objects/User+.swift
@@ -7,11 +7,27 @@
 
 import Foundation
 
+@inline(__always)
+private func _avatar(_ asset: HashedAsset?, size: Int?, discrim: String, id: Snowflake) -> URL {
+    if let url = asset?.avatarURL(of: id, size: size) {
+        return url
+    }
+    let index = discrim == "0"
+        ? ((UInt(id) ?? 0) >> 22) % 6
+        : (UInt(discrim) ?? 0) % 5
+    // If user is without a set avatar, display one of the default ones.
+    // These do not have support for custom sizes.
+    return URL(string: "\(DiscordKitConfig.default.cdnURL)embed/avatars/\(index).png")!
+}
+
 public extension User {
     func avatarURL(size: Int? = nil) -> URL {
-        avatar?.avatarURL(of: id, size: size)
-        // If user is without a set avatar, display one of the default ones.
-        // These do not have support for custom sizes.
-        ?? URL(string: "\(DiscordKitConfig.default.cdnURL)embed/avatars/\((Int(discriminator) ?? 0) % 5).png")!
+        _avatar(avatar, size: size, discrim: discriminator, id: id)
+    }
+}
+
+public extension CurrentUser {
+    func avatarURL(size: Int? = nil) -> URL {
+        _avatar(avatar, size: size, discrim: discriminator, id: id)
     }
 }
