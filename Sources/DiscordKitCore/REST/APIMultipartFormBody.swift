72508d205e6846013ce4ba2fed8c198e7be68e9c
Jack Hogan <jackhogan11@gmail.com>
2025-08-13 11:16:32 -0700

    AttachableData fully public

--- Sources/DiscordKitCore/REST/APIMultipartFormBody.swift
+++ Sources/DiscordKitCore/REST/APIMultipartFormBody.swift
@@ -26,6 +26,12 @@ public struct AttachableData {
     public let data: Data
     public let name: String
     public let mimeType: String
+
+	public init(data: Data, name: String, mimeType: String) {
+        self.data = data
+        self.name = name
+        self.mimeType = mimeType
+    }
 }
 
 extension URL: Attachable {
